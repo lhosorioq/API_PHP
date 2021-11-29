371c001f19a51fab43282a6d0317e69d08e6b142
Luis Humberto Osorio Quiceno <lhosorio@gmail.com>
2021-11-29 09:35:07 -0500

    Configuracion de la gestion a la base de datos.

--- clases/conexion/conexion.php
+++ clases/conexion/conexion.php
@@ -1,6 +1,7 @@
 <?php
 
 class conexion {
+    
     private $server;
     private $user;
     private $password;
@@ -8,16 +9,16 @@ class conexion {
     private $port;
     private $conexion;
 
-    function __construct()
-    {
+    function __construct(){
         $listadatos = $this->datosConexion();
-        foreach($listadatos as $key => $value){
+        foreach ($listadatos as $key => $value) {
             $this->server = $value['server'];
             $this->user = $value['user'];
             $this->password = $value['password'];
             $this->database = $value['database'];
             $this->port = $value['port'];
         }
+
         $this->conexion = new mysqli(
             $this->server,
             $this->user,
@@ -25,15 +26,55 @@ class conexion {
             $this->database,
             $this->port
         );
-        if($this->conexion->connect_errno) {
-            echo "Algo salio mal en la conexion";
-            die;
+
+        if($this->conexion->connect_errno){
+            echo "Algo va mal con la conexion";
+            die();
         }
     }
 
-    private function datosConexion() {
+    private function datosConexion(){
         $direccion = dirname(__FILE__);
         $jsondata = file_get_contents($direccion . "/" . "config");
         return json_decode($jsondata, true);
+    }
+
+    private function convertirUTF8($array){
+        array_walk_recursive($array, function(&$item, $key){
+            if(!mb_detect_encoding($item, 'utf8', true)){
+                $item = utf8_encode($item);
+            }
+        });
+        return $array;
+    }
+
+    public function obtenerDatos($sqlstr){
+        $results = $this->conexion->query($sqlstr);
+        $resultArray = array();
+        foreach ($results as $key) {
+            $resultArray[] = $key;
+        }
+        return $this->convertirUTF8($resultArray);
+    }
+
+    public function nonQuery($sqlstr){
+        $results = $this->conexion->query($sqlstr);
+        return $this->conexion->affected_rows;
+    }
+
+    //INSERT 
+    public function nonQueryId($sqlstr){
+        $results = $this->conexion->query($sqlstr);
+        $filas = $this->conexion->affected_rows;
+        if($filas >= 1){
+        return $this->conexion->insert_id;
+        }else{
+            return 0;
+        }
+    }
+    
+    //encriptar
+    protected function encriptar($string){
+        return md5($string);
     }
 }
--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
-require_once "class/conexion/conexion.php";
+require_once "clases/conexion/conexion.php";
 
 $conexion = new conexion;
+
+$query = "INSERT INTO pacientes ('DNI') VALUES ('G000000007');";
+print_r($conexion->nonQueryId($query));
+
+// $query = "select * from pacientes ";
+// print_r($conexion->obtenerDatos($query));
